c699e56e14a09e846ee6f832986ddc03ae84276c
Javier Monjaraz <193231@ids.upchiapas.edu.mx>
2022-06-03 09:55:47 -0500

    cors

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const cors = require('cors')
 
 const libros = require('./Routes/libros')
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
