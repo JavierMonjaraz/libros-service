cb74ea2548912d1515329fbde123181e48efb1ef
Javier Monjaraz <193231@ids.upchiapas.edu.mx>
2022-06-29 11:48:13 -0500

    add buildspec.yml, gitignore config and port changes

--- app.js
+++ app.js
@@ -5,17 +5,12 @@ const cors = require('cors')
 
 const libros = require('./Routes/libros')
 
-const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.use('/api/v1',libros)
 
-app.listen(5000, () => {
-    console.log("Servidor levantado en el puerto: 5000")
+app.listen(3000, () => {
+    console.log("Servidor levantado en el puerto: 3000")
 })
